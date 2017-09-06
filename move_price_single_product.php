49bff0a73cb4997a49bd328c1c8cec2a1fbcb57a
akramulhasan <lighthouse8048@gmail.com>
2017-09-06 21:05:47 +0600

    move price section from single product page

---
+++ move_price_single_product.php
@@ -0,0 +1,5 @@
+<?php
+
+// Move WooCommerce price
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 25 );
