a21a2f119c9c25a7ada666f57589e687fec9d9b4
AKRAMUL HASAN <akramul@AKRAMULs-MacBook-Pro.local>
2022-07-28 08:16:08 +0600

    product single page only form logged in user

---
+++ product_single_page_only_for_logged_in_user.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This function will prevent guest user to have access of product single page.
+ *
+ *
+ * @author Akramul Hasan
+ * @link https://profiles.wordpress.org/wpfy
+ */
+
+function wpfy_access_product_to_member_only(){
+
+	if(!is_user_logged_in() && is_singular('product')){
+		//Redirect to the homepage
+		//wp_safe_redirect(home_url());
+
+
+		//Redirect to woocommerce my account page
+		wp_safe_redirect(wc_get_page_permalink('myaccount'));
+	}
+
+}
+add_action(template_redirect,wpfy_access_product_to_member_only);
